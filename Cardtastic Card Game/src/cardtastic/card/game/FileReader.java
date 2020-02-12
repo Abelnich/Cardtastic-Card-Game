52cca0ad5bfb5ed21d3f98383de738eb75d196ed
Nick Abel <AbelNich@msu.edu>
2020-02-12 14:52:56 -0500

    Updated FileReader

--- Cardtastic Card Game/src/cardtastic/card/game/FileReader.java
+++ Cardtastic Card Game/src/cardtastic/card/game/FileReader.java
@@ -5,10 +5,32 @@
  */
 package cardtastic.card.game;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author NickAbel
  */
 public class FileReader {
+    
+    private String fileName;
+    
+    public FileReader(String fileName) {
+        this.fileName = fileName;
+    }
+    
+    public ArrayList<String> readFile() {
+        ArrayList<String> contents = new ArrayList();
+        
+        
+        
+        return contents;
+    }
+    
+    public void writeFile(ArrayList<String> writeMe) {
+        
+        
+        
+    }
     
 }
