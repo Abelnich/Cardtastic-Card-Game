842a1e4679bb8f1bfd4c8c7579f9c755ece31fb5
Nick Abel <AbelNich@msu.edu>
2020-03-11 14:37:05 -0400

    Set 2 hands to display, with one being hidden

--- Cardtastic Card Game/src/cardtastic/card/game/CrazyEights.java
+++ Cardtastic Card Game/src/cardtastic/card/game/CrazyEights.java
@@ -9,9 +9,14 @@ import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -37,13 +42,54 @@ public class CrazyEights extends Application {
             cpuHand_2.add(myDeck.deal());
             cpuHand_3.add(myDeck.deal());
         }
-        for (int i = 0; i < playerHand.size(); i++) {
-            System.out.println("My Hand: " + " " + playerHand.get(i).getInfo());
+        
+        HBox cpu2HB = new HBox();
+        cpu2HB.setAlignment(Pos.CENTER);
+        cpu2HB.setSpacing(20);
+        for (int i = 0; i < cpuHand_2.size(); i++) {
+            ImageView iv = createIV();
+            cpu2HB.getChildren().add(iv);
         }
         
-        StackPane root = new StackPane();
+        HBox midHB = new HBox();
+        midHB.setAlignment(Pos.CENTER);
+        midHB.setSpacing(200);
         
-        Scene scene = new Scene(root, 300, 250);
+        VBox cpu1VB = new VBox();
+        cpu1VB.setAlignment(Pos.CENTER);
+        for (int i = 0; i < cpuHand_1.size(); i++) {
+            ImageView iv = createIV();
+            iv.setRotate(-90);
+            cpu1VB.getChildren().add(iv);
+        }
+        
+        VBox cpu3VB = new VBox();
+        cpu3VB.setAlignment(Pos.CENTER);
+        for (int i = 0; i < cpuHand_3.size(); i++) {
+            ImageView iv = createIV();
+            iv.setRotate(90);
+            cpu3VB.getChildren().add(iv);
+        }
+        
+        midHB.getChildren().addAll(cpu1VB, cpu3VB);
+        
+        HBox pHandHB = new HBox();
+        pHandHB.setAlignment(Pos.CENTER);
+        pHandHB.setSpacing(20);
+        
+        VBox screenVB = new VBox();
+        screenVB.setAlignment(Pos.CENTER);
+        screenVB.getChildren().addAll(cpu2HB, pHandHB);
+        
+        for (int i = 0; i < playerHand.size(); i++) {
+            // Add player cards to the screen
+            ImageView iv = createIV(playerHand.get(i));
+            pHandHB.getChildren().add(iv);
+        }
+        
+        StackPane root = new StackPane();       
+        root.getChildren().add(screenVB);
+        Scene scene = new Scene(root, 1200, 700);
         
         primaryStage.setTitle("Crazy Eights");
         primaryStage.setScene(scene);
@@ -53,6 +99,24 @@ public class CrazyEights extends Application {
     /**
      * @param args the command line arguments
      */
+    
+    public ImageView createIV(Card c) {
+        ImageView iv = new ImageView(c.getImageFile());
+        iv.setFitHeight(150.2);
+        iv.setFitWidth(100);
+        iv.setPreserveRatio(true);
+        return iv;
+    }
+    
+    public ImageView createIV() {
+        Image back = new Image("resources/CardBack_Blue.png");
+        ImageView iv = new ImageView(back);
+        iv.setFitHeight(150.2);
+        iv.setFitWidth(100);
+        iv.setPreserveRatio(true);
+        return iv;
+    }
+    
     public static void main(String[] args) {
         launch(args);
     }
