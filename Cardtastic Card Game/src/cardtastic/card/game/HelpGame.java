412e051ce3f123e24a664391940391fede8437cd
Nick Abel <Abelnich@msu.edu>
2020-04-13 13:05:13 -0400

    Added text guide for how to play all games.

--- Cardtastic Card Game/src/cardtastic/card/game/HelpGame.java
+++ Cardtastic Card Game/src/cardtastic/card/game/HelpGame.java
@@ -59,6 +59,7 @@ public class HelpGame extends Application {
         hb_iv.setAlignment(Pos.CENTER);
         hb_iv.setSpacing(20);
         hb_iv.getChildren().addAll(iv_guide1, iv_guide2, iv_guide3, iv_guide4);
+        hb_iv.setVisible(false);
         
         lbl_message = new Label("Select below which game you want to know more about.");
         lbl_message.setFont(new Font("calibre", 40));
@@ -100,32 +101,39 @@ public class HelpGame extends Application {
     private void handleButton(Button b) {
         switch (b.getText()) {
             case "Solitaire":
-                lbl_message.setText("Your text here.");
+                lbl_message.setText("The object of the game is to get all the cards off the main board by stacking them based on their respective suits. Each stack is organized from least"
+                        + " to most, with Ace being the low card and King being the highest. On the main board, the player can move the uncovered card around and place it on another if "
+                        + "the next card is one higher in ranking (or a blank in the case of the king) and is also the opposite color. The player may draw from the deck if no moves are "
+                        + "available. The game ends if/when all the suit stacks are completed.");
                 break;
             case "Freecell":
-                lbl_message.setText("Your text here.");
+                lbl_message.setText("The objective of the game is to fill all the home cells in the top right by stacking cards in ascending order in stacks based on their ranks. "
+                        + "The player may also opt to store some cards in the free cell in the top left to free up other cards underneath. The game ends when all the stacks are completed.");
                 break;
             case "Blackjack":
-                lbl_message.setText("Your text here.");
+                lbl_message.setText("The object of the game is to get the sum of your cards as close to 21 as you can without going over and to have a higher score than the dealer. "
+                        + "The Ace is worth either 1 or 11, whichever is higher without going over. The face cards are each worth 10. "
+                        + "The rest of the cards are worth their respective values. The dealer and player both start with 2 cards and can choose to \"Hit\" or \"Stay\" by pressing the "
+                        + "respective buttons.");
                 break;
             case "War":
-                lbl_message.setText("Your text here.");
+                lbl_message.setText("The object of the game is to get your opponent to have no cards in their hand. Each turn, the top card of each hand is played. "
+                        + "Whoever's card is ranked higher takes both cards. The cards are ranked from 2-K, with face cards being worth more than the one under it, and As are the highest."
+                        + " If both players play the same card, a war is declared. The players then place more cards down until someone wins. The game ends when one hand is depleted.");
                 break;
-            case "Crazy Eights":
-                
+            case "Crazy Eights":   
                 lbl_message.setText("The object of the game is to get rid of all your cards. You do so by playing a card that matches either the suit or the "
-                        + "value of the card on the discard pile. To begin, press the start button. Each computer will take it's turn and then the red circle "
+                        + "value of the card on the discard pile. To begin, press the start button. Each computer will take its turn and then the red circle "
                         + "will turn green to indicate it is your turn to go. You may either play a card or draw. Eights are wild! If you play on, you will need "
-                        + "to select a new suit to play with. Once your turn is over, click \"Next Turn\". Repeat until someone wins.");
-                lbl_message.setFont(new Font("calibre", 24));
-                lbl_message.setTextFill(Color.WHITE);
-                lbl_message.setTextAlignment(TextAlignment.CENTER);
+                        + "to select a new suit to play with. Once your turn is over, click \"Next Turn\". Repeat until someone runs out of cards and is declared the winner.");
                 break;
             default:
                 System.out.println("help " + b.getText());
                 break;
         }
-        
+        lbl_message.setFont(new Font("calibre", 24));
+        lbl_message.setTextFill(Color.WHITE);
+        lbl_message.setTextAlignment(TextAlignment.CENTER);
         lbl_message.setWrapText(true);
                 
     }
